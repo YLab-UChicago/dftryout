c5ed10c9c0451a8571ceb48c42951333624978b5
Cyrus <zhouzk@uchicago.edu>
2023-04-09 18:16:17 +0000

    shear ws

--- arm_programs/3x3/b256/src/ws_os0_is0.cpp
+++ arm_programs/3x3/b256/src/ws_os0_is0.cpp
@@ -7,26 +7,17 @@
 #include <algorithm>
 
 using namespace std;
-void binarize(float *inputMatrix, int input_size, int *binarizedMatrix)
-{
-    for (int i = 0; i < input_size; i++)
-    {
-        binarizedMatrix[i] = (int)((unsigned int)inputMatrix[i] >> 31);
-    }
-}
 
-int xnor_popcount(int a, int b)
-{
-    return __builtin_popcount(~(a ^ b));
-}
+
 
 int main(int argc, char *argv[])
 {
-     /* Please type the following in the command line
+
+    /* Please type the following in the command line
         ./<program_name> <input_height> <input_width> <input_depth (plase use 256)> <num_filters>
     */
 
-    FILE *pFile = fopen("../durations/is_os0_ws0.txt", "a");
+    FILE *pFile = fopen("../durations/ws_os0_is0.txt", "a");
     int height;
     int width;
     int depth;
@@ -39,7 +30,6 @@ int main(int argc, char *argv[])
     int64_t *inputs;
     int *outputs;
     int64_t *filters;
-    int idx;
 
     int output_depth;
     int pool_size;
@@ -48,6 +38,7 @@ int main(int argc, char *argv[])
 
     std::clock_t c_start;
     std::clock_t c_end;
+    int layer_counter = 0;
     double time_elapsed_ms;
     height = atoi(argv[1]);
     width = atoi(argv[2]);
@@ -64,26 +55,23 @@ int main(int argc, char *argv[])
     filters = (int64_t *)malloc(sizeof(int64_t) * filter_height * filter_width * num_filters * depth / 64);
     uint64x2x2_t data1;
     uint64x2x2_t data2;
+    
+
     c_start = std::clock();
 
-    for (int f = 0; f < num_filters; f++)
-    {
-        for (int h = 0; h < height; h++)
-        {
-            for (int w = 0; w < width; w++)
-            {
-                idx = (h * width * depth + w * depth)/64;             
-                data1 = vld1q_u64_x2((const uint64_t *) &inputs[idx]);
-                for (int i = 0; i < filter_height; i++)
-                {
-                    for (int j = 0; j < filter_width; j++)
-                    {
-                        int output_h = (h + padding - i) / strides;
-                        int output_w = (w + padding - j) / strides;
-                        data2 = vld1q_u64_x2((const uint64_t *)&filters[(f * filter_height * filter_width * depth + i * filter_width + j)*depth/64]);
+
+    for (int f = 0; f < num_filters; f++) {
+        for (int i = 0; i < filter_height; i ++) {
+            for (int j = 0; j < filter_width; j ++) {
+                data2 = vld1q_u64_x2((const uint64_t *) &filters[f * filter_height * filter_width + i * filter_width + j]);
+                for (int h = 0; h < out_height; h++) {
+                    for (int w = 0; w < out_width; w++) {
+                        int input_h = h * strides + i - padding;
+                        int input_w = w * strides + j - padding;
+                        data1 = vld1q_u64_x2((const uint64_t *) &inputs[(input_h * width * depth /256 + input_w * depth /256) * depth /64]);
                         data1.val[0] = veorq_u64(data1.val[0], data2.val[0]);
                         data1.val[1] = veorq_u64(data1.val[1], data2.val[1]);
-                        outputs[output_h * out_width * num_filters + output_w * num_filters + f] +=  256 - 2 * (vaddvq_u8(vcntq_u8(vreinterpretq_u8_u64(data1.val[0]))) + vaddvq_u8(vcntq_u8(vreinterpretq_u8_u64(data1.val[1]))));
+                        outputs[h * out_width * num_filters + w * num_filters + f] += 256 - 2 * (vaddvq_u8(vcntq_u8(vreinterpretq_u8_u64(data1.val[0]))) + vaddvq_u8(vcntq_u8(vreinterpretq_u8_u64(data1.val[1]))));
                     }
                 }
             }
